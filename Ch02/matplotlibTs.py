e22854df46019109c75cd8e4228c00474dba128e
dengkun2018 <dengkun2018@aliyun.com>
2018-10-23 14:55:14 +0800

    在branch上提交

---
+++ Ch02/matplotlibTs.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+__author__ = 'dengkun'
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
+plt.show()
